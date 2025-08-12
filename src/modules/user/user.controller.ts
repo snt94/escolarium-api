dd46f425db10a9a290264b373df7fd6d31eb91b3
snt94 <eduardo775445@gmail.com>
2025-08-12 19:14:05 -0300

    Add Controllers('UserController')

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,42 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { JwtGuard } from "../auth/guard"
 import { GetUser } from "./decorators/get-user.decorator";
 import { User } from "@prisma/client";
+import { CreateUserDto, UpdateUserDto } from "./dto";
+import { UserService } from "./user.service";
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private readonly userService: UserService) { }
+
     @Get('me')
-    getMe(@GetUser() user: User){
-
+    getMe(@GetUser() user: User) {
         return user;
+    }
 
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(+id);
+    }
+
+    @Post()
+    create(@Body() dto: CreateUserDto) {
+        return this.userService.create(dto);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+        return this.userService.update(+id, dto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.userService.delete(+id);
     }
 }
